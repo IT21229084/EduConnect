309168dff53c2212756e93553ce11a891aac88a6
Malith Iroshan <99582850+IT21229084@users.noreply.github.com>
2024-04-24 20:47:43 +0530

    database

--- course-Service/index.js
+++ course-Service/index.js
@@ -1,13 +1,31 @@
-const express = require('express');
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import express from "express"
 const app = express();
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("connect to MongoDB")
+    } catch (error) {
+        throw error
+        console.log(" Not connect to MongoDB")
+    }
+};
+
+app.use(cors())
+app.use(express.json())
 
 // Define routes
 app.get('/', (req, res) => {
-    res.send('Hello course Service');
+    res.send('Hello course service');
 });
 
 // Start server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
+    connect()
     console.log(`Server is running on port ${port}`);
 });
--- learner-Service/index.js
+++ learner-Service/index.js
@@ -1,5 +1,22 @@
-const express = require('express');
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import express from "express"
 const app = express();
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("connect to MongoDB")
+    } catch (error) {
+        throw error
+        console.log(" Not connect to MongoDB")
+    }
+};
+
+app.use(cors())
+app.use(express.json())
 
 // Define routes
 app.get('/', (req, res) => {
@@ -9,5 +26,6 @@ app.get('/', (req, res) => {
 // Start server
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
+    connect()
     console.log(`Server is running on port ${port}`);
 });
