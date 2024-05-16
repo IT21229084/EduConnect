5d963daa20757c146e62393ee1d7f96a360ecf64
IT21278280 <99583379+IT21278280@users.noreply.github.com>
2024-05-16 09:55:33 +0530

    Update Service
    Updated

--- learner-Service/BACKEND/frontend/src/components/inc/header.js
+++ learner-Service/BACKEND/frontend/src/components/inc/header.js
@@ -56,13 +56,18 @@ export default function Header() {
               </a>
             </li>
             <li className="nav-item header-link">
-              <a className="nav-link fw-bold header-link" href="# ">
+              <a className="nav-link fw-bold header-link" href="/student-management">
                 Memberships
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link fw-bold header-link" href="# ">
-                Contact Us
+              <a className="nav-link fw-bold header-link" href="/course-delivery-management ">
+                Course Delivery
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link fw-bold header-link" href="/progress-tracking ">
+                Progress Check
               </a>
             </li>
             <li className="nav-item">
